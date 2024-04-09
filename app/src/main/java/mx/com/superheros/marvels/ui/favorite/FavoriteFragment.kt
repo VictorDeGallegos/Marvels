e56595e096bcbeacfb01c4aacea8f6e0cd3f287d
VictorGallegos <316160456@ciencias.unam.mx>
2024-04-09 13:57:05 -0600

    feat(UI):Change nav bar

--- app/src/main/java/mx/com/superheros/marvels/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/mx/com/superheros/marvels/ui/favorite/FavoriteFragment.kt
@@ -1,4 +1,4 @@
-package mx.com.superheros.marvels.ui.notifications
+package mx.com.superheros.marvels.ui.favorite
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import mx.com.superheros.marvels.databinding.FragmentNotificationsBinding
+import mx.com.superheros.marvels.databinding.FragmentFavoriteBinding
 
-class NotificationsFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentFavoriteBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val favoriteViewModel =
+            ViewModelProvider(this).get(FavoriteViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        favoriteViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
